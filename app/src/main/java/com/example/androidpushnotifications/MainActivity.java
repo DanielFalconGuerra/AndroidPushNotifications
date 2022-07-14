22df9f5d5f09a1a6f554aa8e88c4d9f90dba3ee3
dfalcon <daniel.falcon@ethanallen.com>
2022-07-14 15:33:17 -0500

    Get Token added

--- app/src/main/java/com/example/androidpushnotifications/MainActivity.java
+++ app/src/main/java/com/example/androidpushnotifications/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.androidpushnotifications;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if (!task.isSuccessful()) {
+                        Log.e("TAG", "FCM registration token failed", task.getException());
+                        return;
+                    }
+                    String token = task.getResult();
+                    //String msg = getString(R.string.msg_token_fmt, token);
+                    Log.e("TAG", token);
+                });
     }
 }
