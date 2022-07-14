7b0217e5444a64f6fa2c77991106982c87b83b11
Daniel <DanielFalcon.87@gmail.com>
2022-07-13 23:58:33 -0500

    Initial commit

---
+++ app/src/main/java/com/example/androidpushnotifications/PushNotificationService.java
@@ -0,0 +1,8 @@
+package com.example.androidpushnotifications;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class PushNotificationService extends FirebaseMessagingService {
+
+}
